96ccfed8f2be632215525e54fa06c83aa18c59d7
khizersh <moh.khizer61@gmail.com>
2023-06-15 13:44:24 +0500

    khizer before merging

--- src/components/File.js
+++ src/components/File.js
@@ -11,12 +11,12 @@ import { useHistory } from "react-router-dom";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import {saveAs} from "file-saver";
-import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { saveAs } from "file-saver";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
-const File = ({ data, onClick }) => {
+const File = ({ data, onClick, viewType }) => {
   const router = useHistory();
   const [file, setFile] = useState(null);
 
@@ -60,7 +60,7 @@ const File = ({ data, onClick }) => {
     }
   };
   const onClickShare = () => {
-      router.push("/resource-share?id=" + file?._id);
+    router.push("/resource-share?id=" + file?._id);
   };
   const onClickDownload = () => {
     // router.push("/resource-download?id=" + file?._id);
@@ -68,193 +68,192 @@ const File = ({ data, onClick }) => {
   };
 
   return (
-    // <a className="card folderLayputCardImage" onClick={onClickResource}>
-    //   <div className="hoverDiv">
-    //     <div className="iconActive">
-    //       <div className="checkBox">
-    //         <FormGroup>
-    //           <FormControlLabel control={<Checkbox />} />
-    //         </FormGroup>
-    //       </div>
-    //       <div className="iconMenus">
-    //         <FileDownloadIcon
-    //           onMouseOver={() => setOnHover(true)}
-    //           onClick={(e) => onClickDownload(true)}
-    //           onMouseLeave={() => setOnHover(false)}
-    //           style={{ marginRight: "10px" }}
-    //         />
-    //         <ShareIcon
-    //           onMouseOver={() => setOnHover(true)}
-    //           onClick={(e) => onClickShare(true)}
-    //           onMouseLeave={() => setOnHover(false)}
-    //         />
-    //       </div>
-    //     </div>
-    //     <div className="eyeIcon">
-    //       <VisibilityIcon />
-    //     </div>
-    //   </div>
+    <>
+      {viewType == "list" ? (
+        <div
+          className="card folderLayputCardImage-col"
+          onClick={onClickResource}
+        >
+          <div className="card-imgaa">
+            {file?.mimeType?.includes("video") ? (
+              <img src="https://drive-app.s3.amazonaws.com/c7c1feb8-a5b3-4acb-8d39-bdeae261ae7f.png" />
+            ) : (
+              <img src={file?.file} />
+            )}
+          </div>
 
-    //   <div className="card-imgaa">
-    //     {file?.mimeType?.includes("video") ? (
-    //       <img src="https://drive-app.s3.amazonaws.com/c7c1feb8-a5b3-4acb-8d39-bdeae261ae7f.png" />
-    //     ) : (
-    //       <img src={file?.file} />
-    //     )}
-    //   </div>
+          <div className="describation">
+            <div className="_titleColumnContainer_1u6yab5">
+              <div
+                className="_x8ede33 file-name"
+                data-id="re_single_download_action"
+              >
+                <div className="_truncate_title">
+                  <span>happyEndTagWink_v2_COPY REMOVAL</span>
+                </div>
+              </div>
+              <div className="_secondaryText_jnfj5l">
+                <div>
+                  <span className="_labelText_hq5slr">File Format:</span> Video
+                  (mov)
+                </div>
+                <div>
+                  <span className="_labelText_hq5slr">File Size:</span> 176.83
+                  MB
+                </div>
+              </div>
 
-    //   <div className="card-describation">
-    //     <div class="_countContainer_13ovesk">
-    //       <div class="_truncateMulti_3ywtd5">
-    //         <span>
-    //           {file?.folderCount || file?.resourceCount ? (
-    //             <div className="_countContainer_13ovesk">
-    //               <div className="_truncateMulti_3ywtd5">
-    //                 <span>
-    //                   <i>
-    //                     {file?.folderCount ? (
-    //                       file?.folderCount + " Sub-Folders"
-    //                     ) : (
-    //                       <></>
-    //                     )}{" "}
-    //                     {file?.resourceCount ? (
-    //                       file?.resourceCount + " , Resources"
-    //                     ) : (
-    //                       <></>
-    //                     )}
-    //                   </i>
-    //                 </span>
-    //               </div>
-    //             </div>
-    //           ) : (
-    //             <></>
-    //           )}
-    //         </span>
-    //       </div>
-    //     </div>
-    //     <div class="_bottom_f93tfg">
-    //       <div class="_dpv7do3">
-    //         <div class="_truncate_ww5d6d">
-    //           <span>{file?.name}</span>
-    //         </div>
-    //       </div>
-    //       <div class="_content_c3jvop">
-    //         <div>
-    //           <span>
-    //             {" "}
-    //             {file?.mimeType?.includes("video") ? (
-    //               "Video (mov)"
-    //             ) : (
-    //               <>{file?.width + " x " + file?.height}</>
-    //             )}{" "}
-    //             {file?.fileSize}
-    //           </span>
-    //         </div>
-    //       </div>
-    //       <div className="_linkColours_11bsm43">
-    //         <Public color="#4a4a4a" />
-    //       </div>
-    //     </div>
-    //   </div>
-    // </a>
-    <div className="card folderLayputCardImage-col" onClick={onClickResource}>
+              <div className="_indicators_lp6zce">
+                <div className="_indicatorRow_5kaapu">
+                  <a className="_linkColours_crf4gc">
+                    <button className="_iconButton_10fc6mf" type="button">
+                      <span className="material-icons _icon_77bp8u ">
+                        <Public />
+                      </span>
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
 
-    <div className="card-imgaa">
-      {file?.mimeType?.includes("video") ? (
-        <img src="https://drive-app.s3.amazonaws.com/c7c1feb8-a5b3-4acb-8d39-bdeae261ae7f.png" />
+          <div className="card-date">
+            <span>6/5/2022</span>
+          </div>
+
+          <div className="_actionsColumn_13waoy8">
+            <div className="actions-menu ">
+              <div className="" style={{ paddingRight: 24 }}>
+                <div className="_container_1892uua ">
+                  <a className="_linkColours_icon">
+                    <button className="_iconButton_10fc6mfw" type="button">
+                      <span className="material-icons _icon_77bp8u ">
+                        <OndemandVideoIcon />
+                      </span>
+                    </button>
+                    <div className="_text_13v1tkt">Watch</div>
+                  </a>
+                </div>
+              </div>
+              <div className="" style={{ paddingRight: 24 }}>
+                <div className="_container_1892uua ">
+                  <a className="_linkColours_icon">
+                    <button className="_iconButton_10fc6mfw" type="button">
+                      <span className="material-icons _icon_77bp8u ">
+                        <FileDownloadIcon />
+                      </span>
+                    </button>
+                    <div className="_text_13v1tkt">Download</div>
+                  </a>
+                </div>
+              </div>
+              <div className="" style={{ paddingRight: 24 }}>
+                <div className="_container_1892uua ">
+                  <a className="_linkColours_icon">
+                    <button className="_iconButton_10fc6mfw" type="button">
+                      <span className="material-icons _icon_77bp8u ">
+                        <MoreVertIcon />
+                      </span>
+                    </button>
+                    <div className="_text_13v1tkt">More</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       ) : (
-        <img src={file?.file} />
-      )}
-    </div>
+        <a className="card folderLayputCardImage" onClick={onClickResource}>
+          <div className="hoverDiv">
+            <div className="iconActive">
+              <div className="checkBox">
+                <FormGroup>
+                  <FormControlLabel control={<Checkbox />} />
+                </FormGroup>
+              </div>
+              <div className="iconMenus">
+                <FileDownloadIcon
+                  onMouseOver={() => setOnHover(true)}
+                  onClick={(e) => onClickDownload(true)}
+                  onMouseLeave={() => setOnHover(false)}
+                  style={{ marginRight: "10px" }}
+                />
+                <ShareIcon
+                  onMouseOver={() => setOnHover(true)}
+                  onClick={(e) => onClickShare(true)}
+                  onMouseLeave={() => setOnHover(false)}
+                />
+              </div>
+            </div>
+            <div className="eyeIcon">
+              <VisibilityIcon />
+            </div>
+          </div>
 
-    <div className="describation">
-    <div className="_titleColumnContainer_1u6yab5">
-  <div className="_x8ede33 file-name" data-id="re_single_download_action">
-    <div className="_truncate_title">
-      <span>happyEndTagWink_v2_COPY REMOVAL</span>
-    </div>
-  </div>
-  <div className="_secondaryText_jnfj5l">
-    <div>
-      <span className="_labelText_hq5slr">File Format:</span> Video (mov)
-    </div>
-    <div>
-      <span className="_labelText_hq5slr">File Size:</span> 176.83 MB
-    </div>
-  </div>
+          <div className="card-imgaa">
+            {file?.mimeType?.includes("video") ? (
+              <img src="https://drive-app.s3.amazonaws.com/c7c1feb8-a5b3-4acb-8d39-bdeae261ae7f.png" />
+            ) : (
+              <img src={file?.file} />
+            )}
+          </div>
 
-  <div className="_indicators_lp6zce">
-    <div className="_indicatorRow_5kaapu">
-          <a className="_linkColours_crf4gc">
-            <button
-              className="_iconButton_10fc6mf" type="button">
-                <span
-                  className="material-icons _icon_77bp8u ">
-                 <Public/>
+          <div className="card-describation">
+            <div class="_countContainer_13ovesk">
+              <div class="_truncateMulti_3ywtd5">
+                <span>
+                  {file?.folderCount || file?.resourceCount ? (
+                    <div className="_countContainer_13ovesk">
+                      <div className="_truncateMulti_3ywtd5">
+                        <span>
+                          <i>
+                            {file?.folderCount ? (
+                              file?.folderCount + " Sub-Folders"
+                            ) : (
+                              <></>
+                            )}{" "}
+                            {file?.resourceCount ? (
+                              file?.resourceCount + " , Resources"
+                            ) : (
+                              <></>
+                            )}
+                          </i>
+                        </span>
+                      </div>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
                 </span>
-            </button>
-          </a>
-    </div>
-  </div>
-
-</div>
-
-    </div>
-
-    <div className="card-date">
-      <span>6/5/2022</span>
-    </div>
-
-    <div className="_actionsColumn_13waoy8">
-    <div className="actions-menu ">
-  <div className="" style={{ paddingRight: 24 }}>
-    <div className="_container_1892uua ">
-      <a className="_linkColours_icon">
-        <button className="_iconButton_10fc6mfw" type="button">
-            <span
-              className="material-icons _icon_77bp8u ">
-              <OndemandVideoIcon />
-            </span>
-        </button>
-        <div className="_text_13v1tkt">Watch</div>
-      </a>
-    </div>
-  </div>
-  <div className="" style={{ paddingRight: 24 }}>
-    <div className="_container_1892uua ">
-      <a className="_linkColours_icon">
-        <button
-          className="_iconButton_10fc6mfw"
-          type="button">
-            <span
-              className="material-icons _icon_77bp8u " >
-              <FileDownloadIcon />
-            </span>
-        </button>
-        <div className="_text_13v1tkt">Download</div>
-      </a>
-    </div>
-  </div>
-  <div className="" style={{ paddingRight: 24 }}>
-    <div className="_container_1892uua ">
-      <a className="_linkColours_icon">
-        <button
-          className="_iconButton_10fc6mfw"
-          type="button">
-            <span
-              className="material-icons _icon_77bp8u " >
-              <MoreVertIcon />
-            </span>
-        </button>
-        <div className="_text_13v1tkt">More</div>
-      </a>
-    </div>
-  </div>
- 
-</div>
-
-    </div>
-  </div>
+              </div>
+            </div>
+            <div class="_bottom_f93tfg">
+              <div class="_dpv7do3">
+                <div class="_truncate_ww5d6d">
+                  <span>{file?.name}</span>
+                </div>
+              </div>
+              <div class="_content_c3jvop">
+                <div>
+                  <span>
+                    {" "}
+                    {file?.mimeType?.includes("video") ? (
+                      "Video (mov)"
+                    ) : (
+                      <>{file?.width + " x " + file?.height}</>
+                    )}{" "}
+                    {file?.fileSize}
+                  </span>
+                </div>
+              </div>
+              <div className="_linkColours_11bsm43">
+                <Public color="#4a4a4a" />
+              </div>
+            </div>
+          </div>
+        </a>
+      )}
+    </>
   );
 };
 
